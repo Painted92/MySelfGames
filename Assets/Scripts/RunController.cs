38ff0ee2fb0da67c6998ebe3fed5f25eaccfc346
Painted92 <111422701+Painted92@users.noreply.github.com>
2023-01-11 16:33:59 +0200

    StartComit

---
+++ Assets/Scripts/RunController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
+public class RunController : MonoBehaviour
+{
+    [SerializeField] Rigidbody rigidbody;
+    [SerializeField] FixedJoystick joystick;
+    [Header("Speed element")]
+    [SerializeField] private float movespeed;
+
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = new Vector3(joystick.Horizontal * movespeed, rigidbody.velocity.y, joystick.Vertical * movespeed);
+        if(joystick.Horizontal != 0 || joystick.Vertical !=0)
+        {
+            transform.rotation = Quaternion.LookRotation(rigidbody.velocity);
+        }
+    }
+}
